b0f830ac1b95dc03f5f3f546c05e79bb0bf6e7f0
David <david.loh18.dl@gmail.com>
2026-01-20 19:05:20 +0100

    adjusted build config and publish script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,30 +1,30 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    platform: "neutral",
-    entry: ["src/index.ts"],
-    format: ["esm", "cjs", "iife"],
-    target: "es2019",
-    sourcemap: true,
-    clean: true,
-    dts: true, // generate .d.ts files
-    outExtension(ctx) {
-        let extension = "";
-        switch (ctx.format) {
-            case "cjs":
-                extension = "cjs";
-                break;
-            case "esm":
-                extension = "mjs";
-                break;
-            case "iife":
-                extension = "global.js";
-                break;
-            default:
-                extension = `${ctx.format}.js`;
-        }
-        return {
-            js: `.${extension}`,
-        }
+  platform: "neutral",
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs", "iife"],
+  target: "es2019",
+  sourcemap: false,
+  clean: true,
+  dts: true, // generate .d.ts files
+  outExtension(ctx) {
+    let extension = "";
+    switch (ctx.format) {
+      case "cjs":
+        extension = "cjs";
+        break;
+      case "esm":
+        extension = "mjs";
+        break;
+      case "iife":
+        extension = "global.js";
+        break;
+      default:
+        extension = `${ctx.format}.js`;
     }
+    return {
+      js: `.${extension}`,
+    };
+  },
 });
